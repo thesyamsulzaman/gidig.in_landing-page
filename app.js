4f40530d36dcf11e6f42fe7615a5098e1e36346f
Syamsul <thesyamsulzaman@gmail.com>
2020-07-10 09:42:47 +0700

    Adding Files and Navbar

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let mainNav = document.getElementById("js-menu");
+  let navBarToggle = document.getElementById("js-navbar-toggle");
+
+  navBarToggle.addEventListener("click", function () {
+    //mainNav.classList.toggle(".nav__list--active");
+    mainNav.classList.toggle("active");
+  });
+
+  Resources;
+});
