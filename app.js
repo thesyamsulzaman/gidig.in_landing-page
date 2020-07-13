67e8204dfe64ef39bbc223e2eeb706b6da8dc47d
Syamsul <thesyamsulzaman@gmail.com>
2020-07-13 10:37:36 +0700

    Redesigned hero and the navbar

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let mainNav = document.getElementById("js-menu");
+  //let mainNav = document.getElementById("js-menu");
+  let navListOne = document.querySelector(".nav__list--one");
+  let navListTwo = document.querySelector(".nav__list--two");
   let navBarToggle = document.getElementById("js-navbar-toggle");
 
   navBarToggle.addEventListener("click", function () {
-    mainNav.classList.toggle("nav__list--active");
+    navListOne.classList.toggle("nav__list--toggle");
+    navListTwo.classList.toggle("nav__list--toggle");
   });
 });
