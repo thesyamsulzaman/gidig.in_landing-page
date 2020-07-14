c2443a8d83bee9fdbfa3e452d2be5ab3bdccb45a
Syamsul <thesyamsulzaman@gmail.com>
2020-07-14 17:11:09 +0700

    adding service section

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
   let navListOne = document.querySelector(".nav__list--one");
   let navListTwo = document.querySelector(".nav__list--two");
   let navBarToggle = document.getElementById("js-navbar-toggle");
+  const nav = document.querySelector("header");
+  let body = document.body,
+    docElem = document.documentElement,
+    offset = 100,
+    scrollPos,
+    docHeight;
+
+  window.addEventListener("scroll", function () {
+    nav.className = docElem.scrollTop > 22 ? "nav--background" : "";
+  });
 
   navBarToggle.addEventListener("click", function () {
     navListOne.classList.toggle("nav__list--toggle");
